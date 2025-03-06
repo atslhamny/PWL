773eb671c82bb50320a92ebae3e2b485c602150e
Atsilah Amany <atsilahamanyph@gmail.com>
2025-03-06 12:17:50 +0700

    JBS 2: Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return ('Hello World');
+});
+
+
+Route::get('/world', function () {
+    return ('World');
+});
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return ('Selamat Datang');
+});
+
+
+Route::get('/about', function () {
+    return ('Nama : Atsilah Amany Putri Harsuma <br>
+            NIM : 2341760011');
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return ('Pos ke-' . $postId . " Komentar ke-: ". $commentId);
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return ('Halaman Artikel dengan ID ' . $id);
+});
+
+Route::get('user/{name?}', function ($name = 'John') {
+    return ('Nama Saya ' . $name);
 });
